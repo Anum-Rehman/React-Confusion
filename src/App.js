e55ca1f1bf2417e5217aad66e666e461df179078
Anum-Rehman <anumr32@gmail.com>
2019-11-11 13:37:35 +0500

    React Component Part-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar } from 'react-bootstrap';
 import './App.css';
 import Menu from './components/MenuComponent'
+import { DISHES } from './shared/dishes'
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+render(){
   return (
     <div>
     <Navbar bg="primary" variant="dark">
@@ -11,9 +20,9 @@ function App() {
         <Navbar.Brand href="/">Ristorante Con Fusion</Navbar.Brand>
       </div>
     </Navbar>
-    <Menu/>
+    <Menu dishes={this.state.dishes}/>
   </div>
   );
 }
-
+}
 export default App;
